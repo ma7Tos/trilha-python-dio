61a7852406bdc010b50cadd1032c1c635b1a2eaa
Felipe Mattos <felipe_mtt@outlook.com>
2024-04-15 17:52:12 -0300

    arquivos das teste_aulas

---
+++ 01 - Estrutura de dados/01 - Listas/teste_aulas.py
@@ -0,0 +1,10 @@
+usuarios = {
+    "felipeaguiardemattosgoncalves@gmail.com": {"nome": "Felipe", "idade": 29, "genero": "Masculino", "telefone": "11984852859"},
+    "patydlcinha@hotmail.com": {"nome": "Patricia", "idade": 39, "genero": "Feminino", "telefone": "11920736969"},
+    "mar7070@gmail.com": {"nome": "Marisa", "idade": 18, "genero": "Feminino", "telefone": "11970707070"},
+    "ismael_sujeira@outlook.com": {"nome": "Ismael", "idade": 27, "genero": "Masculino", "telefone": "11915935728"},
+}
+
+lista: list = usuarios.keys()
+
+print(f"", lista)
