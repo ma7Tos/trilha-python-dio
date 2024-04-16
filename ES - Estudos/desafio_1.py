d6efd3cfceb126fe587fef7468f04737760372c9
Felipe Mattos <felipe_mtt@outlook.com>
2024-04-16 18:07:20 -0300

    desafio_1 rv01

--- ES - Estudos/desafio_1.py
+++ ES - Estudos/desafio_1.py
@@ -3,6 +3,6 @@ print("Quantos passos você vai dar?")
 quantidade_passos = int(input())
 
 for i in range(quantidade_passos):
-    print("Você deu {} passos."(quantidade_passos))
+    print(f"voce deu {i} passos.")
     if quantidade_passos == 0:
         print("Nenhum passo dado na floresta.")
